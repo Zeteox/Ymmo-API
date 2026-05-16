0600a2ec2487083658762a3ba03287d41ad50bae
Zeteox <zeteox01@gmail.com>
2026-05-16 14:44:55 +0200

    initial commit

---
+++ src/main/java/fr/loic_delprat/ymmo_api/YmmoApiApplication.java
@@ -0,0 +1,13 @@
+package fr.loic_delprat.ymmo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmmoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmmoApiApplication.class, args);
+	}
+
+}
